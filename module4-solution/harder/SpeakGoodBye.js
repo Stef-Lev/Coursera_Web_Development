8c14201aa084c3a8e5ac3b613e5d92c88bb387ab
Stefanos Leventis <stefanos.leventis@upstreamsystems.com>
2020-09-17 19:14:52 +0300

    module 4 solved

---
+++ module4-solution/harder/SpeakGoodBye.js
@@ -0,0 +1,9 @@
+
+(function (){
+  var byeSpeaker = {speak: function(name){
+    console.log(speakWord + " " + name);
+  }}
+  var speakWord = "Good Bye";
+
+  window.byeSpeaker = byeSpeaker;
+})();
