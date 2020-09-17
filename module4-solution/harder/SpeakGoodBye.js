b6569037311461e4fd27577b20dd64fd83cd4667
Stefanos Leventis <stefanos.leventis@upstreamsystems.com>
2020-09-17 19:21:19 +0300

    solved and changed folders

--- module4-solution/harder/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-
-(function (){
-  var byeSpeaker = {speak: function(name){
-    console.log(speakWord + " " + name);
-  }}
-  var speakWord = "Good Bye";
-
-  window.byeSpeaker = byeSpeaker;
-})();
